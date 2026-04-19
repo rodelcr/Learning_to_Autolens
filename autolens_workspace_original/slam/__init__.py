9562693652ca9fd486c2caaafbf389808de77775
rodelcr <rodelcr@gmail.com>
2026-04-18 22:38:58 -0400

    Revert "Make subhalo import defensive in workspace slam package"
    This reverts commit 006556134d2f184f151decec662c73ef1067b618.

--- autolens_workspace_original/slam/__init__.py
+++ autolens_workspace_original/slam/__init__.py
@@ -3,13 +3,5 @@ from . import source_pix
 from . import light_lp
 from . import mass_total
 from . import mass_light_dark
-
-# `subhalo` transitively imports al.AdaptImageMaker which was removed
-# in autolens 2026.x. Import defensively so the rest of this package
-# stays usable; re-enable once upstream catches up.
-try:
-    from . import subhalo
-except AttributeError:
-    pass
-
+from . import subhalo
 from . import slam_util
